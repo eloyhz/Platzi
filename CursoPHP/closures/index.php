801f69c5fa3d75ecb890b6c8a3dabc8e91027bf3
Eloy Hernandez <eloyhz@gmail.com>
2017-06-21 10:48:42 -0500

    Ejemplo de funciones anónimas utilizando la función a través de una variable.

--- CursoPHP/closures/index.php
+++ CursoPHP/closures/index.php
@@ -17,10 +17,14 @@ $var();
 
 $x = 3;
 $numbers = [1, 2, 3, 4, 5];
+$closure = function ($n) use ($x) {
+    return $n * $x;
+};
+
+// el valor de la dependencia no se puede modificar una vez asignado el valor con use
+$x = 4;
 
 // uso de funciones anónimas como parámetros de funciones (callback)
-$result = array_map(function ($n) use ($x) {
-    return $n * $x;
-}, $numbers);
+$result = array_map($closure, $numbers);
 
 var_dump($result);
