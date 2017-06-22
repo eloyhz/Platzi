dfe92a38fb9385842be9e82c7f74032f1d7b21fc
Eloy Hernandez <eloyhz@gmail.com>
2017-06-22 18:28:06 -0500

    Creación de objetos para recibir la notificación cuando el usuario sea logeado.

--- CursoFirebaseAndroid/app/src/main/java/mx/uaslp/eloyhz/firebasecursoplatzi/MainActivity.java
+++ CursoFirebaseAndroid/app/src/main/java/mx/uaslp/eloyhz/firebasecursoplatzi/MainActivity.java
@@ -1,13 +1,43 @@
 package mx.uaslp.eloyhz.firebasecursoplatzi;
 
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+    }
+
+    public void initialize()    {
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            public static final String TAG = "MainActivity";
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    Log.w(TAG, "onAuthStateChanged - signed_in" + firebaseUser.getUid());
+                    Log.w(TAG, "onAuthStateChanged - signed_in" + firebaseUser.getEmail());
+                }
+                else {
+                    Log.w(TAG, "onAuthStateChanged - signed_out" );
+                }
+            }
+        };
     }
 }
