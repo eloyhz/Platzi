be1edbed405ea002def17bff6d3e617be9ff36ab
Eloy Hernandez <eloyhz@gmail.com>
2017-06-22 17:25:47 -0500

    Creacion del proyecto de Firebase para Android

---
+++ CursoFirebaseAndroid/app/src/main/java/mx/uaslp/eloyhz/firebasecursoplatzi/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.uaslp.eloyhz.firebasecursoplatzi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
