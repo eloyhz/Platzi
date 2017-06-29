c636bc1727c62e6de58505d50f23f88a25af858b
Eloy Hernandez <eloyhz@gmail.com>
2017-06-29 17:48:18 -0500

    Área de circulo y cuadrado usando funciones arrow y anónimas.

--- FundamentosJavaScript/Bases/area.js
+++ FundamentosJavaScript/Bases/area.js
@@ -1,9 +1,27 @@
 let base = 5
 let height = 7
 
-let triangleArea = (base, height) => {
-  return base * height / 2
-}
+// función para calcular el área de un triángulo usando una arrow function
+const triangleArea = (base, height) => base * height / 2
 
 console.log(`El área de un triángulo de base
   ${base} y altura ${height} es: ${triangleArea(base, height)}`);
+
+// calcular el área de un círculo usando una arrow function con cuerpo
+
+let radius = 10
+
+const circleArea = (radius) => {
+  return Math.PI * radius * radius
+}
+
+console.log(`El área de un círculo con radio ${radius} es: ${circleArea(radius)}`);
+
+// calcular el área de cun rectangulo usando una función asignada a una variable
+
+let rectangleArea = function (base, height)  {
+  return base * height
+}
+
+console.log(`El área de un rectángulo con base ${base} y altura ${height}
+es: ${rectangleArea(base, height)}`);
