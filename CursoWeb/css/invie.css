f5f65198939a8ca2cfc8d390ac11586a5f6451d1
Eloy Hernandez <eloyhz@gmail.com>
2017-07-18 20:26:23 -0500

    Tipos de selectores: universal, etiqueta, descendente, clase e id.

---
+++ CursoWeb/css/invie.css
@@ -0,0 +1,54 @@
+/*
+
+tipo_de_selector {
+  propiedad: valor;
+  propiedad2: valor;
+}
+*/
+
+/* Selector universal */
+* {
+  border: 1px solid red;
+  margin: 10px;
+}
+
+/* Selector de etiqueta */
+header {
+  background: blue;
+}
+
+section {
+  background: purple;
+}
+
+/* Selector descendente */
+article h3 {
+  color: green;
+}
+
+/* selectores de clase */
+.title-a {
+  background: yellow;
+}
+
+.title-b {
+  background: salmon;
+}
+
+/* selector de id */
+#encabezado {
+  border: 3px solid black;
+}
+
+#portada {
+  border: 3px solid orange;
+}
+
+#guitarras {
+  border: 3px solid brown;
+}
+
+/*
+body {
+  background: peru;
+}*/
